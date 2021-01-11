32407a6fa4d0b8fa03951f4c506d0f6cf46871ab
JuneJH <15310487388@163.com>
2021-01-11 16:50:52 +0800

    调整结构

---
+++ api/upload.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require("path");
+const multer = require('multer');
+
+
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../static/uploads'))
+    },
+    filename: function (req, file, cb) {
+        const ext = path.extname( file.originalname)
+        cb(null, `${file.fieldname}-${Date.now()}${ext}`);
+    }
+})
+
+const upload = multer({ storage: storage })
+
+router.post('/', upload.single('imgName'), async function (req, res, next) {
+    res.json({message: "ok", url: `/uploads/${req.file.filename}`});
+});
+
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,54 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-const multer = require('multer');
-const url = require("url");
 
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, './static/uploads'))
-    },
-    filename: function (req, file, cb) {
-        const ext = path.extname( file.originalname)
-        cb(null, `${file.fieldname}-${Date.now()}${ext}`);
-    }
-})
-
-const upload = multer({ storage: storage })
+const uploadRouter = require("./api/upload")
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.post('/upload', upload.single('imgName'), async function (req, res, next) {
-    res.json({message: "ok", url: `./uploads/${req.file.filename}`});
-});
+app.use("/upload",uploadRouter)
 
-app.get('/download', function (req, res) {
-    const urlParams = url.parse(req.url).query;
-    console.log(urlParams)
-    const filename = urlParams.replace("filename=", "");
-    let file = path.join(__dirname, "./static/uploads/" + filename);
-    res.download(file);
-});
-app.listen(9527)
-
-// const server = http.createServer(async (req, res) => {
-//     const url = req.url;
-//     if (url == "/upload" && req.method === "POST") {
-//         let file = ""
-//         req.on("data", (chunk => file += chunk))
-//         req.on("end", () => {
-//             let fileP = path.join(__dirname, "./uploads/"+"123.jpg");
-//             fs.promises.writeFile(fileP, file).then(value => {
-//                 console.log("OK")
-//                 res.end("0k")
-//             })
-//         });
-//
-//
-//     }
-// });
-// server.on('clientError', (err, socket) => {
-//     socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-// });
-// server.listen(9527);
+app.listen(9527,()=>{console.info("start server succeed")})
